c67c27bb56bff21b869eb7dc48e8f3a759e44b50
hassan1091 <roh.baba.11@gmail.com>
2022-11-07 15:00:09 +0300

    finish the server v-1

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const supplements = require("./supplements.json");
 
 const app = express();
 
@@ -13,11 +14,48 @@ app.use(
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-  res.render(
-    "home" /*, {
-      content : homeStartingContent,
-    }*/
+  res.render("home");
+});
+
+app.get("/category/:category", (req, res) => {
+  let categorySupplements = supplements.filter(
+    (supplement) => supplement.type === req.params.category
   );
+  res.render("category", {
+    supplements: categorySupplements,
+  });
+});
+
+app.get("/category/:category/:category2", (req, res) => {
+  let categorySupplements = supplements.filter(
+    (supplement) => supplement.type === req.params.category
+  );
+  let categoryTowSupplements = supplements.filter(
+    (supplement) => supplement.type === req.params.category2
+  );
+  res.render("category", {
+    supplements: categorySupplements,
+    secondarySupplements: categoryTowSupplements,
+  });
+});
+
+app.get("/category/:category/info/:name", (req, res) => {
+  let supplement = supplements.find(
+    (supplement) =>
+      supplement.type === req.params.category &&
+      supplement.name === req.params.name
+  );
+  res.render("supplement info", {
+    supplement: supplement,
+  });
+});
+
+app.get("/recommended", (req, res) => {
+  res.render("recommended stores");
+});
+
+app.get("/data", (req, res) => {
+  res.send(JSON.stringify(supplements));
 });
 
 app.listen(3000, () => {
